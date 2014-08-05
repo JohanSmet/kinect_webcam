JohanSmet/kinect_webcam

commit 4ae8b4749e5b42fe50a28cb571432862243d36f1

- check for valid device before using it - disconnect the device when not in use and reconnect when streaming starts

src/device_kinect_v2.cpp
@@ -219,6 +219,10 @@ bool DeviceKinectV2::disconnect()
 		com_safe_release(&m_private->m_sensor);
 	}
 
+	m_private->m_color_data.clear();
+	m_private->m_depth_data.clear();
+	m_private->m_body_index_data.clear();
+
 	return true;
 }
 
@@ -280,13 +284,17 @@ Point2D	DeviceKinectV2::focus_point()
 
 bool DeviceKinectV2::update()
 {
+	// sensor connected ?
+	if (!m_private->m_sensor)
+		return false;
+
 	// read the color frame separately - the kinect can drop to 15fps in low light conditions 
 	//	but we don't want to delay the other data sources
 	bool f_new_data = read_color_frame();
 
 	// check if there's new data available in the multi-source reader
 	com_safe_ptr_t<IMultiSourceFrame>	f_multi_frame = nullptr;
-	if (SUCCEEDED(m_private->m_sensor_multi_reader->AcquireLatestFrame(&f_multi_frame)))
+	if (m_private->m_sensor_multi_reader && (m_private->m_sensor_multi_reader->AcquireLatestFrame(&f_multi_frame)))
 	{ 
 		f_new_data |= read_body_index_frame(f_multi_frame.get());
 		f_new_data |= read_body_frame(f_multi_frame.get());
@@ -302,6 +310,9 @@ bool DeviceKinectV2::update()
 
 bool DeviceKinectV2::color_data(int p_hor_focus, int p_ver_focus, int p_width, int p_height, int p_bpp, unsigned char *p_data)
 {
+	if (m_private->m_color_data.empty())
+		return false;
+
 	if (p_width  > m_private->m_color_width  ||
 	    p_height > m_private->m_color_height)
 	{
@@ -332,6 +343,9 @@ bool DeviceKinectV2::color_data(int p_hor_focus, int p_ver_focus, int p_width, i
 
 bool DeviceKinectV2::read_color_frame()
 {
+	if (!m_private->m_sensor_color_reader)
+		return false;
+
 	com_safe_ptr_t<IColorFrame> f_frame = nullptr;
 
 	// try to read the next frame
@@ -349,6 +363,9 @@ bool DeviceKinectV2::read_body_index_frame(IMultiSourceFrame *p_multi_source_fra
 {
 	com_safe_ptr_t<IBodyIndexFrameReference> f_frame_ref = nullptr;
 	com_safe_ptr_t<IBodyIndexFrame>			 f_frame = nullptr;
+
+	if (!p_multi_source_frame)
+		return false;
 
 	// try to read the next frame
 	auto f_result = p_multi_source_frame->get_BodyIndexFrameReference(&f_frame_ref);
@@ -370,6 +387,9 @@ bool DeviceKinectV2::read_body_frame(IMultiSourceFrame *p_multi_source_frame)
 {
 	com_safe_ptr_t<IBodyFrameReference> f_frame_ref = nullptr;
 	com_safe_ptr_t<IBodyFrame>			f_frame = nullptr;
+
+	if (!p_multi_source_frame)
+		return false;
 
 	// try and read the next frame
 	auto f_result = p_multi_source_frame->get_BodyFrameReference(&f_frame_ref);
@@ -422,8 +442,5 @@ bool DeviceKinectV2::read_body_frame(IMultiSourceFrame *p_multi_source_frame)
 
 	return SUCCEEDED(f_result);
 }
-
-
-
 
 } // namespace device

src/filter_video.cpp
@@ -133,13 +133,26 @@ CKCamStream::CKCamStream(HRESULT *phr, CKCam *pParent, LPCWSTR pPinName) :
 	}
 
 	// initialize the camera focus in the center of the camera
-	auto f_native_res = m_device->video_resolution(m_device->video_resolution_native());
-	m_focus.m_x = f_native_res.m_width / 2;
-	m_focus.m_y = f_native_res.m_height / 2;
+	if (m_device)
+	{
+		auto f_native_res = m_device->video_resolution(m_device->video_resolution_native());
+		m_focus.m_x = f_native_res.m_width / 2;
+		m_focus.m_y = f_native_res.m_height / 2;
+	}
+
+	// disconnect from the device until playback is started
+	if (m_device)
+	{
+		m_device->disconnect();
+	}
 }
 
 CKCamStream::~CKCamStream()
 {
+	if (m_device)
+	{
+		m_device->disconnect();
+	}
 } 
 
 HRESULT CKCamStream::QueryInterface(REFIID riid, void **ppv)
@@ -166,6 +179,9 @@ HRESULT CKCamStream::QueryInterface(REFIID riid, void **ppv)
 HRESULT CKCamStream::FillBuffer(IMediaSample *pms)
 {
 	const REFERENCE_TIME AVG_FRAME_TIME = (reinterpret_cast<VIDEOINFOHEADER*> (m_mt.pbFormat))->AvgTimePerFrame;
+
+	if (!m_device)
+		return E_FAIL;
 
 	// get the current time from the reference clock	
 	IReferenceClock *f_clock = nullptr;
@@ -268,6 +284,9 @@ HRESULT CKCamStream::GetMediaType(int iPosition, CMediaType *pmt)
 {
 	DbgLog((LOG_TRACE, 1, "GetMediaType (iPosition = %d)", iPosition));
 
+	if (!m_device)
+		return E_FAIL;
+
     if (iPosition < 0) return E_INVALIDARG;
     if (iPosition > m_device->video_resolution_count()) return VFW_S_NO_MORE_ITEMS;
 
@@ -321,6 +340,9 @@ HRESULT CKCamStream::CheckMediaType(const CMediaType *pMediaType)
 	//	=> we just check some crucial parameters of the requested media type match with something we offer
 	DbgLog((LOG_TRACE, 1, "CheckMediaType"));
 
+	if (!m_device)
+		return E_FAIL;
+
     DECLARE_PTR(VIDEOINFOHEADER, f_pvi, pMediaType->pbFormat);
 	DbgLog((LOG_TRACE, 1, "... CheckMediaType (%dx%dx%d)", f_pvi->bmiHeader.biWidth, f_pvi->bmiHeader.biHeight, f_pvi->bmiHeader.biBitCount));
 
@@ -372,15 +394,28 @@ HRESULT CKCamStream::OnThreadCreate()
 	m_num_dropped = 0;
 	m_num_frames  = 0;
 
+	// be sure to refresh the settings
+	settings::load();
+
+	// reconnect to the device
+	if (m_device)
+	{
+		m_device->connect_to_first();
+	}
+
     return NOERROR;
 }
 
 HRESULT CKCamStream::OnThreadDestroy()
 {
-	settings::cleanup();
+	// disconnect from the device
+	if (m_device)
+	{
+		m_device->disconnect();
+	}
 
-	if (!m_device->disconnect())
-		return E_FAIL;
+	// stop monitoring settings
+	settings::cleanup();
 
     return NOERROR;
 }
@@ -399,6 +434,9 @@ HRESULT STDMETHODCALLTYPE CKCamStream::SetFormat(AM_MEDIA_TYPE *pmt)
 	// If the pin is already connected, and the pin supports the media type, reconnect the pin with that type. 
 	//	If the other pin rejects the new type, return VFW_E_INVALIDMEDIATYPE and restore the original connection.
 	DbgLog((LOG_TRACE, 1, "SetFormat : %x", pmt));
+
+	if (!m_device)
+		return E_FAIL;
 
 	if (!pmt)
 	{
@@ -438,6 +476,9 @@ HRESULT STDMETHODCALLTYPE CKCamStream::GetFormat(AM_MEDIA_TYPE **ppmt)
 
 HRESULT STDMETHODCALLTYPE CKCamStream::GetNumberOfCapabilities(int *piCount, int *piSize)
 {
+	if (!m_device)
+		return E_FAIL;
+
     *piCount = m_device->video_resolution_count();
     *piSize = sizeof(VIDEO_STREAM_CONFIG_CAPS);
     return S_OK;
@@ -445,6 +486,9 @@ HRESULT STDMETHODCALLTYPE CKCamStream::GetNumberOfCapabilities(int *piCount, int
 
 HRESULT STDMETHODCALLTYPE CKCamStream::GetStreamCaps(int iIndex, AM_MEDIA_TYPE **pmt, BYTE *pSCC)
 {
+	if (!m_device)
+		return E_FAIL;
+
     *pmt = CreateMediaType(&m_mt);
     DECLARE_PTR(VIDEOINFOHEADER, pvi, (*pmt)->pbFormat);
 

commit 68c15ebec4f14345a5326da46ab838ccd91125e6

Commit 4ae8b474 broke joint tracking on kinect v2

src/device_kinect_v2.cpp
@@ -298,7 +298,7 @@ bool DeviceKinectV2::update()
 
 	// check if there's new data available in the multi-source reader
 	com_safe_ptr_t<IMultiSourceFrame>	f_multi_frame = nullptr;
-	if (m_private->m_sensor_multi_reader && (m_private->m_sensor_multi_reader->AcquireLatestFrame(&f_multi_frame)))
+	if (m_private->m_sensor_multi_reader && SUCCEEDED (m_private->m_sensor_multi_reader->AcquireLatestFrame(&f_multi_frame)))
 	{ 
 		f_new_data |= read_body_index_frame(f_multi_frame.get());
 		f_new_data |= read_body_frame(f_multi_frame.get());